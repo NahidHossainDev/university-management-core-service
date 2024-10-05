539602bdf24aa174d54215686accd66b262e62de
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-10-05 14:29:44 +0600

    remove generation log file for development

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,31 @@
+/* eslint-disable no-unused-expressions */
 import { Server } from 'http';
 import app from './app';
 import config from './config';
 import { errorlogger, logger } from './shared/logger';
 
-
 async function bootstrap() {
-
   const server: Server = app.listen(config.port, () => {
-    logger.info(`Server running on port ${config.port}`);
+    config.env === 'development'
+      ? console.log(`Server running on port ${config.port}`)
+      : logger.info(`Server running on port ${config.port}`);
   });
 
   const exitHandler = () => {
-
     if (server) {
       server.close(() => {
-        logger.info('Server closed');
+        config.env === 'development'
+          ? console.log('Server closed')
+          : logger.info('Server closed');
       });
     }
     process.exit(1);
   };
 
   const unexpectedErrorHandler = (error: unknown) => {
-    errorlogger.error(error);
+    config.env === 'development'
+      ? console.log(error)
+      : errorlogger.error(error);
     exitHandler();
   };
 
@@ -29,7 +33,9 @@ async function bootstrap() {
   process.on('unhandledRejection', unexpectedErrorHandler);
 
   process.on('SIGTERM', () => {
-    logger.info('SIGTERM received');
+    config.env === 'development'
+      ? console.log('SIGTERM received')
+      : logger.info('SIGTERM received');
     if (server) {
       server.close();
     }
