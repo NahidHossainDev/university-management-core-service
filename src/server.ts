35273b887a2331e6ece311af65de14777bc7d9d5
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-10-21 17:02:23 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import { Server } from 'http';
 import app from './app';
 import config from './config';
 import { errorlogger, logger } from './shared/logger';
+import { RedisClient } from './shared/redis';
 
 async function bootstrap() {
+  await RedisClient.connect();
   const server: Server = app.listen(config.port, () => {
     config.env === 'development'
       ? console.log(`Server running on port ${config.port}`)
